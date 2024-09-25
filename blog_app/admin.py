b65b73435a5104af2e9b6c08bcc224cd4e9dbb34
Bikash Chowdury <chowdhurybikash38@gmail.com>
2024-09-25 12:10:10 +0600

    create model post

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog_app.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','title','description']
+
+admin.site.register(Post,PostAdmin)
